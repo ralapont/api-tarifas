ffc4adabdf5c64d0816c54681a276a75e9aa5d9a
ralapont <rafael.alapont.gomez@gmail.com>
2023-01-09 23:27:03 +0100

    first commit

---
+++ src/main/java/com/example/apitarifas/ApiTarifasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitarifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTarifasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTarifasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apitarifas/ApiTarifasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitarifas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTarifasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
