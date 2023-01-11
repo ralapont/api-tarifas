f4eb6b2f401cc1c5257a3e8d346995113ffa12c5
ralapont <rafael.alapont.gomez@gmail.com>
2023-01-11 14:01:10 +0100

    castellanizar salidas

--- src/main/java/com/example/apitarifas/dtos/ErrorResponse.java
+++ src/main/java/com/example/apitarifas/dtos/ErrorResponse.java
@@ -1,5 +1,7 @@
 package com.example.apitarifas.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +12,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
+@JsonRootName(value = "error")
 public class ErrorResponse {
+    @JsonProperty(value="status")
     private HttpStatus status;
+    @JsonProperty(value="mensaje")
     private String message;
+    @JsonProperty(value="fechaYHoraError")
     private LocalDateTime timestamp;
+    @JsonProperty(value="erroresList")
     private List<String> errors;
 }
--- src/main/java/com/example/apitarifas/dtos/PricesResponse.java
+++ src/main/java/com/example/apitarifas/dtos/PricesResponse.java
@@ -1,5 +1,7 @@
 package com.example.apitarifas.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,12 +10,19 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
+@JsonRootName(value = "precio")
 public class PricesResponse {
 
+    @JsonProperty(value="producto")
     private Integer productId;
+    @JsonProperty(value="rama")
     private Integer brandId;
+    @JsonProperty(value="listaPrecios")
     private Integer priceList;
+    @JsonProperty(value="fechaComienzo")
     private LocalDateTime startDate;
+    @JsonProperty(value="fechaFin")
     private LocalDateTime endDate;
+    @JsonProperty(value="precio")
     private Double price;
 }
