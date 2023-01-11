681713a25b5205cf465014c714d1476b5092c378
ralapont <rafael.alapont.gomez@gmail.com>
2023-01-11 12:07:13 +0100

    optimice code

--- src/main/java/com/example/apitarifas/controller/PricesControllerAdvice.java
+++ src/main/java/com/example/apitarifas/controller/PricesControllerAdvice.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
@@ -14,12 +13,12 @@ import java.time.LocalDateTime;
 public class PricesControllerAdvice {
 
     @ExceptionHandler(value = NoRateToApplyException.class)
-    public ResponseEntity<ErrorResponse> resourceNotFoundException(NoRateToApplyException ex, WebRequest request) {
+    public ResponseEntity<ErrorResponse> resourceNotFoundException(NoRateToApplyException ex) {
         ErrorResponse message = ErrorResponse.builder()
                 .message(ex.getMessage())
                 .status(HttpStatus.NO_CONTENT)
                 .timestamp(LocalDateTime.now())
                 .build();
-        return new ResponseEntity<ErrorResponse>(message, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 }
